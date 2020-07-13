123301a80d7c4f94dd44275ef0b446f5c6fc0615
thebossjr <68233164+thebossjr@users.noreply.github.com>
2020-07-13 21:03:51 +0700

    Add files via upload

---
+++ ty.py
@@ -0,0 +1,57 @@
+a="\33[0;32m"
+b="\33[1;33m"
+c="\33[0;36m"
+d="\33[31;1m"
+e='\33[31;35m'
+print(e+"""  _____  _    _  _____ ______ _  __
+ |  __ \| |  | |/ ____|  ____| |/ /
+ | |__) | |  | | |    | |__  | ' / 
+ |  ___/| |  | | |    |  __| |  <  
+ | |    | |__| | |____| |____| . \ 
+ |_|     \____/ \_____|______|_|\_\
+ """)
+print(a+"   -----------facebook hack---------\n   ===========!WELCOME!=============\n\npilih 1 untuk hack\n\npilih 2 untuk keluar\n\n=====================")
+def uy():
+    halo=raw_input("masukkan pilihan>>>")
+    opo(halo)
+def opo(cok):
+    if cok=="1":
+        cok=raw_input(c+"\nmasukkan url target>>>")
+    elif cok=="2":
+        exit()
+    else:
+        print("pilihan salah")
+        uy()
+
+             
+uy()
+    
+print b+"masukkan pilihan hack\n\n1.bruteforce\n\n2.multibruteforce"
+
+tulul=raw_input(d+"\nmasukkan pilihan:")
+
+import time
+time.sleep(1.0)
+print(d+"Tunggu Sebentar sedang di proses")
+time.sleep(1.0)
+print("    10%")
+time.sleep(1.0)
+print("          20%")
+time.sleep(1.0)
+print"                30%"
+time.sleep(1.0)
+print"                   40%"
+time.sleep(1.0)
+print"                      50%"
+time.sleep(2.0)
+print"                   60%"
+time.sleep(2.2)
+print"                70%"
+time.sleep(2.3)
+print"             80%"
+time.sleep(2.1)
+print"         90%"
+time.sleep(9.1)
+print"100%"
+time.sleep(10.0)
+print("password dari sandi=Tapi Boong")
